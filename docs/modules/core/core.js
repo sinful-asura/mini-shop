42fb0dbea33d7b52c98bef2b16f2ede7ffe734bb
k <it.kristijan.jovic@gmail.com>
2022-03-15 16:07:51 +0100

    Ayo

--- docs/modules/core/core.js
+++ docs/modules/core/core.js
@@ -8,7 +8,7 @@ let subscribedEvents = [];
 
 document.addEventListener("app-init", () => {
   console.log("%c[Application Initialized]", "color: #0076e3");
-  console.log('Change log');
+  console.log('Change 2');
 });
 
 document.addEventListener("route-changed", () => {
