9abd7d0ef46d041f9842596cf5f64899a4db8d27
k <it.kristijan.jovic@gmail.com>
2022-03-15 15:54:11 +0100

    Abecebe

--- docs/modules/core/core.js
+++ docs/modules/core/core.js
@@ -8,6 +8,7 @@ let subscribedEvents = [];
 
 document.addEventListener("app-init", () => {
   console.log("%c[Application Initialized]", "color: #0076e3");
+  console.log('Change log');
 });
 
 document.addEventListener("route-changed", () => {
